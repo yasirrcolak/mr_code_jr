daf17c138cb376289783cf8a727feb3a66241ed3
Muhammet Yasir Çolak <76009454+yasirrcolak@users.noreply.github.com>
2021-03-28 20:18:57 +0300

    levelsPage added Elevated Butons

--- lib/screens/levels_page.dart
+++ lib/screens/levels_page.dart
@@ -13,46 +13,147 @@ class _LevelsPage extends State {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.amber,
+        color: Colors.pinkAccent,
+
         child: Row(
-          children: [
-            Expanded(
-              child: Text("Ömer Faruk Kral"),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Text("Ömer Faruk Kral"),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(50),
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget> [
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget> [
+                     SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('1', style: TextStyle(fontSize: 20)),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.all(50),
+                     SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('6', style: TextStyle(fontSize: 20)),
+                      ),
                     ),
-                  ),
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('11', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
                 ],
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(50),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget> [
+                  SizedBox(
+                    width: 100.0,
+                    height: 70.0,
+                     child: ElevatedButton(
+                         child: const Text('2', style: TextStyle(fontSize: 20)),
+                       ),
+                     ),
+                  SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('7', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                  SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('12', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                ],
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(50),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget> [
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('3', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('8', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('13', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                ],
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: EdgeInsets.all(50),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget> [
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('4', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('9', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('14', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                ],
               ),
-            ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget> [
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('5', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('10', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 100.0,
+                      height: 70.0,
+                      child: ElevatedButton(
+                        child: const Text('15', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                ],
+              ),
+
           ],
         ),
       ),
